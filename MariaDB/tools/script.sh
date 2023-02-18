cb623d7f5ab99e6b51e045ce34e2f833a9ad38ee
Youssef Badaoui <ybadaoui@e2r4p15.1337.ma>
2023-02-18 18:25:51 +0100

    mariadb dockerfile done

--- MariaDB/tools/script.sh
+++ MariaDB/tools/script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 service mysql start
+sed -i "s/127.0.0.1/0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
 mysql -e "DELETE FROM mysql.user WHERE User='';"
 mysql -e "CREATE DATABASE newdb ;"
-mysql -e "CREATE USER 'newuser'@'%' IDENTIFIED BY 'usery';" 
+mysql -e "CREATE USER newuser IDENTIFIED BY 'usery';"
+mysql -e "GRANT ALL PRIVILEGES ON newdb.* TO newuser;"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'rooty';"
 mysql -e "FLUSH PRIVILEGES;"
-bash
+mysqld_safe
