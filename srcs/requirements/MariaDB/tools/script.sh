c03065683a52202a55ada579aa94b4bfe67b6e28
Youssef Badaoui <ybadaoui@e2r4p15.1337.ma>
2023-02-20 21:18:00 +0100

    mariadb and wordpress installed and connected to each others

--- srcs/requirements/MariaDB/tools/script.sh
+++ srcs/requirements/MariaDB/tools/script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-service mysql start
 
 sed -i "s/127.0.0.1/0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
 
-mysql -e "CREATE DATABASE newdb;"
-mysql -e "CREATE USER 'newuser' IDENTIFIED BY 'usery';"
-mysql -e "GRANT ALL PRIVILEGES ON newdb.* TO 'newuser';"
-mysql -e "FLUSH PRIVILEGES;"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'rooty';"
+service mysql start
 
-# mysqld
-
-bash
+mysql -u root -prooty -e "CREATE DATABASE IF NOT EXISTS newdb;"
+mysql -u root -prooty -e "CREATE USER IF NOT EXISTS 'newuser' IDENTIFIED BY 'usery';"
+mysql -u root -prooty -e "GRANT ALL PRIVILEGES ON newdb.* TO 'newuser';"
+mysql -u root -prooty -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'rooty';"
+mysql -u root -prooty -e "FLUSH PRIVILEGES;"
+kill `cat /var/run/mysqld/mysqld.pid`
+mysqld
+# bash
--- srcs/requirements/WordPress/tools/script.sh
+++ srcs/requirements/WordPress/tools/script.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-# chmod 600 wp-config.php
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+mkdir -p /run/php/
+mkdir -p /var/www/html
+cd /var/www/html
+wp core download --allow-root
+cp wp-config-sample.php wp-config.php
+rm wp-config-sample.php
+chmod 600 wp-config.php
 
 sed -i "s/database_name_here/newdb/" wp-config.php
 sed -i "s/username_here/newuser/" wp-config.php
 sed -i "s/password_here/usery/" wp-config.php
 sed -i "s/localhost/mariadb/" wp-config.php
 
-wp core install --url=http://localhost:80/ --title=tobedefianed --admin_user=wp-admin --admin_password=your_admin_password --admin_email=your_admin_email --allow-root
-php-fpm7.4 -F
+wp core install --url=http://localhost/ --title=tobedefianed --admin_user=wp-admin --admin_password=your_admin_password --admin_email=email@email.com --allow-root
+php-fpm7.3 -F
 # bash
