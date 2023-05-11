b09d53eb9705c3bd608e7e2944c41a88453b3c85
Youssef Badaoui <ybadaoui@e2r11p5.1337.ma>
2023-05-11 14:00:46 +0100

    add volumes

--- srcs/requirements/MariaDB/tools/script.sh
+++ srcs/requirements/MariaDB/tools/script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 sed -i "s/127.0.0.1/0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
 
 service mysql start
--- srcs/requirements/WordPress/tools/script.sh
+++ srcs/requirements/WordPress/tools/script.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
+
+chown -R www-data:www-data /var/www/html/
+chmod -R 755 /var/www/html/
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
-mkdir -p /run/php/
-mkdir -p /var/www/html
-cd /var/www/html
 wp core download --allow-root
 cp wp-config-sample.php wp-config.php
-rm wp-config-sample.php
-chmod 600 wp-config.php
-chown -R www-data:www-data /var/www/html/
-
+# rm wp-config-sample.php
 sed -i "s/database_name_here/$DB_NAME/" wp-config.php
 sed -i "s/username_here/$DB_USER/" wp-config.php
 sed -i "s/password_here/$DB_USER_PASS/" wp-config.php
 sed -i "s/localhost/mariadb/" wp-config.php
-sed -i "s/\/run\/php\/php7.3-fpm.sock/9000/" /etc/php/7.3/fpm/pool.d/www.conf 
-
+sed -i "s/\/run\/php\/php7.3-fpm.sock/9000/" /etc/php/7.3/fpm/pool.d/www.conf
+sleep 5
 wp core install --url=$MY_DOMAIN_NAME --title=MY_blog --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_EMAIL --allow-root
 wp user create $WP_USER $WP_USER_EMAIL --role=$WP_USER_RULE --user_pass=$WP_USER_PASS --allow-root
+# cat wp-config.php
 php-fpm7.3 -F
 # bash
