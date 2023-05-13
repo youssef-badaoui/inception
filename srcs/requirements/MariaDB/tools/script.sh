a837ef13533142b884b5c8716c98d4e20b49b635
Youssef Badaoui <ybadaoui@e2r7p1.1337.ma>
2023-05-13 23:33:25 +0100

    done

--- srcs/requirements/MariaDB/tools/script.sh
+++ srcs/requirements/MariaDB/tools/script.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
-
 sed -i "s/127.0.0.1/0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
-
 service mysql start
-
 mysql -u root -p$DB_ROOT_PASS -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 mysql -u root -p$DB_ROOT_PASS -e "CREATE USER IF NOT EXISTS '$DB_USER' IDENTIFIED BY '$DB_USER_PASS';"
 mysql -u root -p$DB_ROOT_PASS -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER';"
--- srcs/requirements/WordPress/tools/script.sh
+++ srcs/requirements/WordPress/tools/script.sh
@@ -7,7 +7,6 @@ chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 wp core download --allow-root
 cp wp-config-sample.php wp-config.php
-# rm wp-config-sample.php
 sed -i "s/database_name_here/$DB_NAME/" wp-config.php
 sed -i "s/username_here/$DB_USER/" wp-config.php
 sed -i "s/password_here/$DB_USER_PASS/" wp-config.php
@@ -16,6 +15,5 @@ sed -i "s/\/run\/php\/php7.3-fpm.sock/9000/" /etc/php/7.3/fpm/pool.d/www.conf
 sleep 10
 wp core install --url=$MY_DOMAIN_NAME --title=MY_blog --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_EMAIL --allow-root
 wp user create $WP_USER $WP_USER_EMAIL --role=$WP_USER_RULE --user_pass=$WP_USER_PASS --allow-root
-# cat wp-config.php
 php-fpm7.3 -F
 # bash
